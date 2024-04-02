a20ed38c27604696d72ac2b005b1f69cb31eeff0
Nathan Joel <nathanjoel1@hotmail.co.uk>
2024-04-02 11:51:27 +0100

    Tag Component & Styling

---
+++ src/components/TagInput.jsx
@@ -0,0 +1,47 @@
+import React, { useState } from 'react';
+import InputGroup from './InputGroup';
+import "../styles/TagInput.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faX } from '@fortawesome/free-solid-svg-icons';
+
+function TagInput({ tags, addTag, removeTag }) {
+    const [inputValue, setInputValue] = useState('');
+
+    const handleKeyDown = (event) => {
+        if (event.key === 'Enter') {
+            event.preventDefault(); // Prevent form submission
+            if (inputValue) {
+                addTag(inputValue);
+                setInputValue('');
+            }
+        }
+    };
+
+    const handleInputChange = (event) => {
+        setInputValue(event.target.value);
+    };
+
+    return (
+        <div>
+            <div className="tags-input-container">
+                <InputGroup
+                    id="tag-input"
+                    placeholder="Programming 101"
+                    type="text"
+                    labelText="Relevant Modules (enter to add)"
+                    onChange={handleInputChange}
+                    value={inputValue}
+                    onKeyDown={handleKeyDown}
+                />
+                {tags.map((tag, index) => (
+                    <div key={index} className="tag-item" >
+                    {tag}
+                    <button type="button" onClick={() => removeTag(index)}><FontAwesomeIcon icon={faX} /></button>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+}
+
+export default TagInput;
